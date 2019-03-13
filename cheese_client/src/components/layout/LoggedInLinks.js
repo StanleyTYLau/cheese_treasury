5216c36e42979a0989ef4867f868bb8ca8cf5aab
Stanley <stanley.tyl@gmail.com>
2019-03-13 07:01:45 +0000

    navbar setup, loggedin and out links setup

---
+++ cheese_client/src/components/layout/LoggedInLinks.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const LoggedInLinks = () => {
+  return(
+    <ul className="right">
+      <li><NavLink to="/">Cart</NavLink></li>
+      <li><NavLink to="/">Log Out</NavLink></li>
+      <li><NavLink to="/" className="btn btn-floating pink lighten-1">TEST</NavLink></li>      
+    </ul>
+  )
+}
+
+export default LoggedInLinks;
