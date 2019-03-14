43d58bc5c97d5ec1da261d473bb436639d299d96
Stanley <stanley.tyl@gmail.com>
2019-03-14 22:24:53 +0000

    itemdetails retrieving id params

--- cheese_client/src/components/item/ItemDetails.js
+++ cheese_client/src/components/item/ItemDetails.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
-const ItemDetails = () => {
+const ItemDetails = (props) => {
+  const id = props.match.params.id;
+
   return (
     <div className="container section item-details">
       <div className="card z-depth-0">
+
         <div className="card-content">
-          <span className="card-title">Item Title</span>
+          <span className="card-title">Item Title - {id}</span>
           <p>Item Description</p>
         </div>
+
         <div className="card-action grey lighten-5 grey-text">
           <div>Footer</div>
         </div>
+
       </div>
     </div>
   )
