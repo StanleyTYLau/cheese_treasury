9a0b8c9a29dd3cea6f09caacc70750079012bac1
StanleyTYLau <stanley.tyl@gmail.com>
2019-03-18 12:30:16 -0400

    redux version issue

--- cheese_client/src/components/dashboard/Dashboard.js
+++ cheese_client/src/components/dashboard/Dashboard.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import ItemList from '../item/ItemList';
 import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase'
+import { compose } from 'redux'
+
 
 class Dashboard extends Component {
   render() {
@@ -27,4 +30,9 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default connect(mapStateToProps)(Dashboard);
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([
+    { collection: 'items' }
+  ])
+)(Dashboard);
--- cheese_client/src/index.js
+++ cheese_client/src/index.js
@@ -7,19 +7,33 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './store/reducer/rootReducer';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import { reduxFirestore, getFirestore } from 'redux-firestore'
-import { reactReduxFirebase ,getFirebase } from 'react-redux-firebase'
+import firebase from 'firebase/app'
+import { createFirestoreInstance, getFirestore } from 'redux-firestore'
+import { ReactReduxFirebaseProvider ,getFirebase } from 'react-redux-firebase'
 import fbConfig from './config/fbConfig'
 
 const store = createStore(rootReducer, 
   compose(
     applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
-    reduxFirestore(fbConfig),
-    reactReduxFirebase(fbConfig)
+    //reduxFirestore(fbConfig),
+    //ReactReduxFirebase(fbConfig)
   )
 );
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+const rrfProps = {
+  firebase,
+  config: fbConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance // <- needed if using firestore
+}
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <App />
+    </ReactReduxFirebaseProvider>
+  </Provider>,
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
