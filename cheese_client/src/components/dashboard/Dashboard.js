33c8cefceca7881d19a846ed385afeb3c69ab47f
StanleyTYLau <stanley.tyl@gmail.com>
2019-03-18 13:03:58 -0400

    firestore synced

--- cheese_client/src/components/dashboard/Dashboard.js
+++ cheese_client/src/components/dashboard/Dashboard.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import ItemList from '../item/ItemList';
 import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase'
+import { compose } from 'redux'
 
 class Dashboard extends Component {
   render() {
@@ -22,9 +24,15 @@ class Dashboard extends Component {
 }
 
 const mapStateToProps = (state) => {
+  console.log(state);
   return {
-    items: state.item.items
+    items: state.firestore.ordered.items
   }
 }
 
-export default connect(mapStateToProps)(Dashboard);
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([
+    { collection: 'items' }
+  ])
+)(Dashboard);
